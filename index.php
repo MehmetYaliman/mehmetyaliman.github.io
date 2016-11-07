699f6705b6f62cdd9759d8088af3f883c4b09d0c
Mehmet Yaliman <mehmet@yaliman-mbp.local>
2016-11-07 18:06:03 +0100

    site migration

---
+++ index.php
@@ -0,0 +1,114 @@
+﻿<?
+require_once('lib12345654321/mobile_device_detect.php');
+$mobile = mobile_device_detect();
+if($mobile){
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mehmet Yalıman | in short | özetle</title>
+<meta name="keywords" content="mehmet yaliman, mehmet yalıman, personal web site, kişisel site, mehmet yalımanın kişisel sitesi, mehmet yalimans personal site" />
+<meta name="description" content="mehmet yalıman's personal web site" />
+<meta name="author" content="mehmet yalıman" />
+<meta name="distribution" content="global" /> 
+<meta name="verify-v1" content="mzAi3F05hj1t+BgVNB3e4Okq/AwaR2w0QuRzYSr0BIs=" />
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+<link href="my.1.0.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="lib12345654321/iphone.js"></script>
+</head>
+<body onload="">	
+
+	<div id="m_container">
+		<div id="m_image1">
+			<img src="2a65009.jpg" alt="Mehmet Yaliman" width="90" height="90" />	</div>
+		<div id="m_text">
+		  <div id="m_en">
+				<h1>Hi There !</h1>
+				welcome to my minisite.<br />
+				name: mehmet yaliman<br />
+				current position: solution support engineer at <a href="http://www.pingidentity.com">ping identity</a><br />
+				e-mail: mehmet{the sign}yaliman.net<br />
+				online profiles: <a href="http://www.linkedin.com/in/mehmetyaliman">linkedin</a>, <a href="http://www.facebook.com/meyal">facebook</a>, <a href="http://twitter.com/mehmetyaliman">twitter</a><br />
+			</div>
+			<div id="m_tr">
+				<h1>Merhabalar!</h1>
+				mini siteme hoşgeldiniz.<br /> 
+				isim: mehmet yalıman<br />
+				güncel pozisyon: <a href="http://www.pingidentity.com">ping identity</a>'de çözüm destek mühendisi<br />
+				e-posta: mehmet{işaret}yaliman.net<br />
+				çevrimiçi profiller: <a href="http://www.linkedin.com/in/mehmetyaliman">linkedin</a>, <a href="http://www.facebook.com/meyal">facebook</a>, <a href="http://twitter.com/mehmetyaliman">twitter</a><br />
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		copyright &copy; 1981-2016 mehmet yalıman
+	</div>
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+			var pageTracker = _gat._getTracker("UA-3888939-1");
+			pageTracker._trackPageview();
+		} catch(err) {}</script>
+</body>
+</html>
+<?
+}else{
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Mehmet Yalıman | in short | özetle</title>
+<meta name="keywords" content="mehmet yaliman, mehmet yalıman, personal web site, kişisel site, mehmet yalımanın kişisel sitesi, mehmet yalimans personal site" />
+<meta name="description" content="mehmet yalıman's personal web site" />
+<meta name="author" content="mehmet yalıman" />
+<meta name="distribution" content="global" /> 
+<meta name="verify-v1" content="mzAi3F05hj1t+BgVNB3e4Okq/AwaR2w0QuRzYSr0BIs=" />
+<link href="my.1.0.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>	
+
+	<div id="container">
+		<div id="image1">
+			<img src="2a65009.jpg" alt="Mehmet Yaliman!" width="180" height="180" />	</div>
+		<div id="text">
+		  <div id="en">
+				<h1>Hi There !</h1>
+				welcome to my minisite.<br />
+				name: mehmet yaliman<br />
+				current position: solution support engineer at <a href="http://www.pingidentity.com">ping identity</a><br />
+				e-mail: mehmet{the sign}yaliman.net<br />
+				online profiles: <a href="http://www.linkedin.com/in/mehmetyaliman">linkedin</a>, <a href="http://www.facebook.com/meyal">facebook</a>, <a href="http://twitter.com/mehmetyaliman">twitter</a><br />
+			</div>
+			<div id="tr">
+				<h1>Merhabalar!</h1>
+				mini siteme hoşgeldiniz.<br /> 
+				isim: mehmet yalıman<br />
+				güncel pozisyon: <a href="http://www.pingidentity.com">ping identity</a>'de çözüm destek mühendisi<br />
+				e-posta: mehmet{işaret}yaliman.net<br />
+				çevrimiçi profiller: <a href="http://www.linkedin.com/in/mehmetyaliman">linkedin</a>, <a href="http://www.facebook.com/meyal">facebook</a>, <a href="http://twitter.com/mehmetyaliman">twitter</a><br />
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		copyright &copy; 1981-2016 mehmet yalıman
+	</div>
+	<script type="text/javascript">
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+		try {
+			var pageTracker = _gat._getTracker("UA-3888939-1");
+			pageTracker._trackPageview();
+		} catch(err) {}</script>
+</body>
+</html>
+<?
+}
+?>
